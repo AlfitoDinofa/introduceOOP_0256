9364106a5952efd1f0f66d270beadfe240f1bb55
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-04-29 11:21:28 +0700

    menambahkan class

--- class.cpp
+++ class.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa{
 public:
 int nim;
 string nama;
 float nilai;
 
 
+void print_data(){
+    cout << "NIM = " <<nim << endl;
+    cout <<"NAMA = " <<nama <<endl;
+    cout <<"NILAI = " <<nilai <<endl;
+    
+}
+};
 
 
 int main(){
